feb8876236e7029701c3b7e0b485ef646b4cefb4
xinranli <xinranli@andrew.cmu.edu>
2016-02-01 20:38:27 -0500

    head

--- js/index.js
+++ js/index.js
@@ -1,19 +1,12 @@
 
 $(window).scroll(function () {
     var sc = $(window).scrollTop()
-    if (sc > 50) {
+    if (sc > 100) {
         $("nav").addClass('small');
         $(".brand-logo").addClass('small');
         $(".imghead").addClass('small');
-    } else {
-        $("nav").removeClass('small');
-        $(".brand-logo").removeClass('small');
-        $(".imghead").removeClass('small');
-    }
-});
-
-$(document).ready(function() {
-    $("nav").hover(function() {
+        
+        $("nav").hover(function() {
         $("nav").removeClass('small');
         $(".brand-logo").removeClass('small');
         $(".imghead").removeClass('small');
@@ -22,6 +15,12 @@ $(document).ready(function() {
         $(".brand-logo").addClass('small');
         $(".imghead").addClass('small');}
         );
+    } else {
+        $("nav").removeClass('small');
+        $(".brand-logo").removeClass('small');
+        $(".imghead").removeClass('small');
+    }
 });
+
 
     
